9d1db70ba628d5d2b4e4ccfb9451601abbfc871d
Konrad Paziewski <konrad.paziewski@codilime.com>
2015-08-12 16:06:14 +0300

    Added data_type inheritance

--- dsl_parser/elements/data_types.py
+++ dsl_parser/elements/data_types.py
@@ -2,14 +2,24 @@ from dsl_parser import constants
 from dsl_parser import exceptions
 from dsl_parser import utils
 from dsl_parser.elements import properties_utils
-from dsl_parser.framework.elements import Element, Dict
+from dsl_parser.framework.elements import Element, Dict, Leaf, DictElement
 from dsl_parser.framework.parser import parse
 from dsl_parser.framework.requirements import Value
 
 
+_DERIVED_FROM = 'derived_from'
+
+
+class StringElement(Element):
+    schema = Leaf(type=str)
+
+
 class DataType(Element):
     schema = {
-        'properties': properties_utils.UnsafeSchema
+        'properties': properties_utils.UnsafeSchema,
+        'description': StringElement,
+        _DERIVED_FROM: StringElement,
+        'version': StringElement
     }
 
     def parse(self):
@@ -19,6 +29,18 @@ class DataType(Element):
         return result
 
 
+def _add_requirement_if_exists(current_type,
+                               required_type,
+                               types_classes,
+                               unknown_type_err):
+    if required_type not in types_classes:
+        raise exceptions.DSLParsingElementMatchException(
+            39,
+            unknown_type_err)
+    types_classes[current_type].requires[
+        types_classes[required_type]] = [Value(required_type)]
+
+
 class DataTypes(Element):
     schema = Dict(type=DataType)
     provides = ['data_types']
@@ -26,7 +48,7 @@ class DataTypes(Element):
     def parse(self):
         datatypes = self.build_dict_result()
 
-        class DataTypesInternal(Element):
+        class DataTypesInternal(DictElement):
             schema = {}
             requires = {}
 
@@ -38,27 +60,46 @@ class DataTypes(Element):
                     "Illegal type name '{0}' - it is primitive "
                     "type".format(type_name))
 
-            class DataTypeInternal(Element):
-                schema = {
-                    'properties': properties_utils.UnsafeSchema
-                }
+            class DataTypeInternal(DataType):
                 requires = {}
+                _type_name = type_name
 
                 def parse(self, **data_types):
-                    return {
-                        'properties': utils.parse_type_fields(
-                            self.build_dict_result()['properties'],
-                            data_types)
-                    }
+                    schema = self.build_dict_result()
+                    schema['properties'] = utils.parse_type_fields(
+                        schema['properties'],
+                        data_types)
+                    parent_type = schema.get(_DERIVED_FROM)
+                    if parent_type:
+                        schema['properties'] = utils.merge_sub_dicts(
+                            overriding_dict=schema,
+                            overridden_dict=data_types[parent_type],
+                            sub_dict_key='properties'
+                        )
+                    return schema
 
             types_internal[type_name] = DataTypeInternal
             DataTypesInternal.schema[type_name] = DataTypeInternal
 
         for type_name, type_schema in datatypes.iteritems():
+            if type_schema.get(_DERIVED_FROM):
+                err_msg = 'Type {0} derives from unknown type {1}'.format(
+                    type_name,
+                    type_schema[_DERIVED_FROM])
+                _add_requirement_if_exists(type_name,
+                                           type_schema['derived_from'],
+                                           types_internal,
+                                           err_msg)
             for prop_name, prop in type_schema['properties'].iteritems():
-                if 'type' in prop and prop['type'] in types_internal:
-                    prop_type = prop['type']
-                    types_internal[type_name].requires[
-                        types_internal[prop_type]] = [Value(prop_type)]
+                if 'type' in prop and \
+                                prop['type'] not in constants.PRIMITIVE_TYPES:
+                    err_msg = 'Property {0} in type {1} ' \
+                              'has unkown type {2}'.format(prop_name,
+                                                           type_name,
+                                                           prop['type'])
+                    _add_requirement_if_exists(type_name,
+                                               prop['type'],
+                                               types_internal,
+                                               err_msg)
 
         return parse(datatypes, DataTypesInternal)
--- dsl_parser/tests/test_data_types.py
+++ dsl_parser/tests/test_data_types.py
@@ -1,14 +1,15 @@
 from dsl_parser.tests.abstract_test_parser import AbstractTestParser
 from dsl_parser import exceptions
 from dsl_parser.exceptions import (
-    DSLParsingFormatException,
+    DSLParsingElementMatchException,
     DSLParsingLogicException
 )
 from dsl_parser.tasks import prepare_deployment_plan
 
 
 class TestDataTypes(AbstractTestParser):
-    def test_unkown_type(self):
+
+    def test_unknown_type(self):
         yaml = self.MINIMAL_BLUEPRINT + """
 data_types:
   pair_type:
@@ -18,7 +19,7 @@ data_types:
       second: {}
 """
         self._assert_dsl_parsing_exception_error_code(
-            yaml, 1, DSLParsingFormatException)
+            yaml, 39, DSLParsingElementMatchException)
 
     def test_simple(self):
         yaml = self.MINIMAL_BLUEPRINT + """
@@ -52,7 +53,7 @@ data_types:
     list_type:
         properties:
             head:
-                type: int
+                type: integer
             tail:
                 type: list_type
                 default:
@@ -82,7 +83,7 @@ data_types:
         self._assert_dsl_parsing_exception_error_code(
             yaml, 106, DSLParsingLogicException)
 
-    def test_unkown_type_in_datatype(self):
+    def test_unknown_type_in_datatype(self):
         yaml = self.MINIMAL_BLUEPRINT + """
 data_types:
   pair_type:
@@ -92,7 +93,7 @@ data_types:
       second: {}
         """
         self._assert_dsl_parsing_exception_error_code(
-            yaml, 1, DSLParsingFormatException)
+            yaml, 39, DSLParsingElementMatchException)
 
     def test_nested_validation(self):
         yaml = """
@@ -164,3 +165,82 @@ data_types:
         parsed = prepare_deployment_plan(self.parse(yaml))
         vm = self.get_node_by_name(parsed, 'vm')
         self.assertEqual('ubuntu', vm['properties']['agent_name'])
+
+    def test_nested_validation(self):
+        yaml = """
+node_templates:
+  n_template:
+    type: n_type
+    properties:
+      n_pair:
+        second:
+          first: 4
+          second: invalid_type_value
+node_types:
+  n_type:
+    properties:
+      n_pair:
+        type: pair_of_pairs_type
+data_types:
+  pair_type:
+    properties:
+      first: {}
+      second:
+        type: integer
+  pair_of_pairs_type:
+    properties:
+      first:
+        type: pair_type
+        default:
+            first: 1
+            second: 2
+      second:
+        type: pair_type
+"""
+        self._assert_dsl_parsing_exception_error_code(yaml, 50)
+
+    def test_derives(self):
+        yaml = """
+node_types:
+    vm_type:
+        properties:
+            agent:
+                type: agent
+            agent_name:
+                type: string
+            agent_key:
+                type: string
+node_templates:
+    vm:
+        type: vm_type
+        properties:
+            agent:
+                connection:
+                    key: /home/ubuntu/id_rsa
+            agent_name: { get_property: [SELF, agent, connection, username] }
+            agent_key: { get_property: [SELF, agent, connection, key] }
+data_types:
+    agent_connection:
+        properties:
+            username:
+                type: string
+                default: ubuntu
+            key:
+                type: string
+                default: ~/.ssh/id_rsa
+    agent:
+        derived_from: agent_installer
+        properties:
+            basedir:
+                type: string
+                default: /home/
+    agent_installer:
+        properties:
+            connection:
+                type: agent_connection
+                default: {}
+"""
+        parsed = prepare_deployment_plan(self.parse(yaml))
+        vm = self.get_node_by_name(parsed, 'vm')
+        self.assertEqual('ubuntu', vm['properties']['agent_name'])
+        self.assertEqual('/home/ubuntu/id_rsa', vm['properties']['agent_key'])
